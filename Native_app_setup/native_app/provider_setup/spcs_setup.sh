512b5ae4ba3dd9d0629cb1cd5fdf34783b09cd26
Piotr Paczewski <piotr.paczewski@snowflake.com>
2025-06-19 16:22:43 +0200

    missed the downloader container

--- Native_app_setup/native_app/provider_setup/spcs_setup.sh
+++ Native_app_setup/native_app/provider_setup/spcs_setup.sh
@@ -6,12 +6,17 @@ REPO_URL=$(snow spcs image-repository url openrouteservice_spcs.core.image_repos
 # command below should display image repository URL, in case of issues get it from here: https://docs.snowflake.com/en/developer-guide/snowpark-container-services/working-with-registry-repository#image-repository-url
 echo $REPO_URL
 
-# open decktor desktop and build and push images:
+# build and push images (make sure you have docker desktop running in the background):
 
 # openrouteservice image
 cd services/openrouteservice
 docker build --rm --platform linux/amd64 -t $REPO_URL/openrouteservice:v9.0.0 .
 docker push $REPO_URL/openrouteservice:v9.0.0 
+
+# downloader image
+cd ../downloader
+docker build --rm --platform linux/amd64 -t $REPO_URL/downloader:v0.0.3 .
+docker push $REPO_URL/downloader:v0.0.3 
 
 # gateway image
 cd ../gateway
